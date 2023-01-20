ecbc9ce1112120df813ab107cd7f3355ca561aa2
danialumer876 <82091264+danialumer876@users.noreply.github.com>
2023-01-20 15:43:37 +0500

    Update frontend.py

--- frontend-docker/frontend.py
+++ frontend-docker/frontend.py
@@ -2,17 +2,21 @@ import os
 from flask import Flask
 import datetime
 import requests
+import pytz
 from datetime import date
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():  
-    response = requests.get('http://internproject-backend:5100/response')
-    e = datetime.datetime.now()
+   
+    backend_url = os.environ.get("backend-service-url")
+    response = requests.get(f"http://{backend_url}:5100/response")
+    
+    pakistan_timezone = pytz.timezone("Asia/Karachi")
+    e = datetime.datetime.now(pakistan_timezone)   
     data = response.json()
-    return f"{e.day}/{e.month}/{e.year} {e.hour}:{e.minute}  Hello {data['name']}"
+    return f"{e.day}/{e.month}/{e.year} {e.hour}:{e.minute} Hello {data['name']}"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5200)
-
